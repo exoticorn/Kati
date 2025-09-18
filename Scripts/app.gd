a98d41b2c0277c8adff012044e082d003d1aca9d
Dennis Ranke <dennis.ranke@gmail.com>
2025-09-18 21:48:40 +0200

    start of playtak connection

--- Scripts/app.gd
+++ Scripts/app.gd
@@ -1,12 +1,46 @@
 extends Control
 
+var playtak: PlayTakInterface
+
+func _ready():
+	playtak = PlayTakInterface.new()
+	playtak.state_changed.connect(_on_playtak_state_changed)
+	add_child(playtak)
+
 func _on_local_game_pressed() -> void:
 	$MainMenu.hide()
 	$LocalGame.show()
-
+	$MenuButton.show()
 
 func _on_local_game_start_game(settings: Dictionary) -> void:
 	$LocalGame.hide()
 	var game = load("res://Scenes/local_game.tscn").instantiate()
 	game.settings = settings
 	$Games.add_child(game)
+
+func _on_seeks_pressed() -> void:
+	$MainMenu.hide()
+	$MenuButton.show()
+	$Seeks.show()
+
+func _on_login_pressed() -> void:
+	if playtak.state == PlayTakInterface.State.ONLINE:
+		playtak.close_connection()
+	else:
+		playtak.login("Guest")
+	$MainMenu/Box/Login.disabled = true
+
+func _on_playtak_state_changed():
+	if playtak.state == PlayTakInterface.State.ONLINE:
+		$MainMenu/Box/Login.text = "Logout " + playtak.username
+		$MainMenu/Box/Seeks.disabled = false
+	else:
+		$MainMenu/Box/Login.text = "Login"
+		$MainMenu/Box/Seeks.disabled = true
+	$MainMenu/Box/Login.disabled = false
+
+
+func _on_menu_button_pressed() -> void:
+	$MenuButton.hide()
+	$Seeks.hide()
+	$MainMenu.show()
---
+++ Scripts/playtak_interface.gd
@@ -0,0 +1,118 @@
+class_name PlayTakInterface extends Node
+
+enum State {
+	OFFLINE,
+	CONNECTING,
+	LOGGING_IN,
+	ONLINE
+}
+
+var ws_url = "ws://localhost:9999/ws"
+
+var ws := WebSocketPeer.new()
+var state := State.OFFLINE
+var user_pw: String
+var username: String
+
+enum ColorChoice { WHITE, BLACK, ANY}
+enum GameType { RATED, UNRATED, TOURNAMENT }
+
+class Seek:
+	var id: int
+	var user: String
+	var size: int
+	var time: int
+	var inc: int
+	var color: ColorChoice
+	var komi: float
+	var flat_count: int
+	var capstone_count: int
+	var game_type: GameType
+	var extra_time_move: int
+	var extra_time: int
+	var bot: bool
+
+var seeks: Array[Seek] = []
+var online_players: Array[String] = []
+
+signal state_changed
+signal seeks_changed
+signal players_changed
+
+func login(upw: String):
+	user_pw = upw
+	if state != State.OFFLINE:
+		close_connection()
+	ws.supported_protocols = PackedStringArray(["binary"])
+	ws.connect_to_url(ws_url)
+	state = State.CONNECTING
+
+func _process(_delta):
+	if state == State.OFFLINE:
+		return
+	
+	ws.poll()
+	match ws.get_ready_state():
+		WebSocketPeer.STATE_CONNECTING:
+			return
+		WebSocketPeer.STATE_CLOSING:
+			return
+		WebSocketPeer.STATE_CLOSED:
+			state = State.OFFLINE # TODO: reconnect when we were ONLINE
+			state_changed.emit()
+			return
+	
+	while ws.get_available_packet_count() > 0:
+		var line = ws.get_packet().get_string_from_ascii().rstrip(" \n")
+		
+		print("> " + line)
+		
+		match Array(line.split(" ")):
+			["Login", "or", "Register"] when state == State.CONNECTING:
+				send("Client Kati")
+				send("Protocol 2")
+				send("Login " + user_pw)
+				state = State.LOGGING_IN
+			["Authentication", "failure"]:
+				printerr("Failed to login to playtak server")
+				close_connection()
+			["Welcome", var uname]:
+				username = uname.left(-1)
+				state = State.ONLINE
+				print("Connected as " + username)
+				state_changed.emit()
+			["Seek", "new", var id, var user, var size, var time, var inc, var color, var half_komi, var flat_count, var capstone_count, var unrated, var tournament, var extra_time_move, var extra_time, var opp, var bot_seek]:
+				if opp != "0" && opp.to_lower() != username.to_lower():
+					return
+				var seek := Seek.new()
+				seek.id = id.to_int()
+				seek.user = user
+				seek.size = size.to_int()
+				seek.time = time.to_int()
+				seek.inc = inc.to_int()
+				seek.color = ColorChoice.WHITE if color == "W" else ColorChoice.BLACK if color == "B" else ColorChoice.ANY
+				seek.komi = half_komi.to_int() / 2.0
+				seek.flat_count = flat_count.to_int()
+				seek.capstone_count = capstone_count.to_int()
+				seek.game_type = GameType.TOURNAMENT if tournament == "1" else GameType.UNRATED if unrated == "1" else GameType.RATED
+				seek.extra_time_move = extra_time_move.to_int()
+				seek.extra_time = extra_time.to_int()
+				seek.bot = bot_seek == "1"
+				seeks.push_back(seek)
+				seeks_changed.emit()
+			["OnlinePlayers", ..]:
+				online_players = []
+				for player in line.right(-14).split(","):
+					online_players.push_back(player.lstrip("[\" ").rstrip("]\"")) # advanced parsing!
+				players_changed.emit()
+
+func send(line: String):
+	print("< " + line)
+	ws.send_text(line)
+
+func close_connection():
+	ws.close()
+	while ws.get_ready_state() != WebSocketPeer.STATE_CLOSED:
+		await get_tree().create_timer(0.1).timeout
+	state = State.OFFLINE
+	state_changed.emit()
