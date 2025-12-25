5e18d70fd8ec2d9e8dc4878fd1195c1f17491e32
Dennis Ranke <dennis.ranke@gmail.com>
2025-12-25 16:41:47 +0100

    add "back to game" button in move list

--- ui/move_list_ui.gd
+++ ui/move_list_ui.gd
@@ -12,16 +12,17 @@ func _ready():
 func update():
 	if !is_inside_tree():
 		return
-	for child in $Grid.get_children():
-		$Grid.remove_child(child)
+	%BackToGame.visible = _move_list.branch_move <= _move_list.moves.size()
+	for child in %Grid.get_children():
+		%Grid.remove_child(child)
 		child.queue_free()
 	if _move_list.moves.is_empty():
 		var move_number = Label.new()
 		move_number.text = "1."
-		$Grid.add_child(move_number)
+		%Grid.add_child(move_number)
 		var move = Label.new()
 		move.text = "---"
-		$Grid.add_child(move)
+		%Grid.add_child(move)
 	else:
 		var display_move = max(0, _move_list.display_move - 1) / 2
 		var move_count = (_move_list.moves.size() + 1) / 2
@@ -30,7 +31,7 @@ func update():
 		for i in range(start, end):
 			var move_number = Label.new()
 			move_number.text = "%d." % (i + 1)
-			$Grid.add_child(move_number)
+			%Grid.add_child(move_number)
 			for j in 2:
 				var move_index = i * 2 + j
 				if move_index < _move_list.moves.size():
@@ -40,4 +41,8 @@ func update():
 					move.text = _move_list.moves[move_index].to_ptn()
 					if move_index + 1 == _move_list.display_move:
 						move.theme_type_variation = "CurrentMoveLabel"
-					$Grid.add_child(move)
+					%Grid.add_child(move)
+
+
+func _on_back_to_game_pressed() -> void:
+	_move_list.step_move(0)
